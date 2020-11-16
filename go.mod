8029a677b5516d3f94bf7da83f27005d1762f0b2
Rodrigo Valente <valentedev.rodrigo@gmail.com>
2020-11-16 20:43:31 -0300

    add $PORT to .env file

--- go.mod
+++ go.mod
@@ -2,3 +2,5 @@
 module github.com/valentedev/go-postgres-heroku
 
 go 1.15
+
+require github.com/joho/godotenv v1.3.0 // indirect
--- main.go
+++ main.go
@@ -5,17 +5,30 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("PORT not defined")
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Olá, %q", html.EscapeString(r.URL.Path))
 	})
 
 	s := &http.Server{
-		Addr:              ":8080",
+		Addr:              ":" + port,
 		ReadHeaderTimeout: 20 * time.Second,
 		ReadTimeout:       10 * time.Minute,
 		WriteTimeout:      2 * time.Minute,
