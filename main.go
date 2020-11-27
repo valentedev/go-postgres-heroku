750ca9bbb8c338d34979276ec33f36730008aded
Rodrigo Valente <valentedev.rodrigo@gmail.com>
2020-11-26 22:09:30 -0300

    mudar .env e pg info para deploy no heroku

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 	//aqui chamamos a func seed() para migrar os dados do []UsuariosDB para Banco de Dados novo.
 	//depois que os dados foram migrados, podem deixar de chamar a função seed()
-	//seed(conect)
+	seed(conect)
 
 	//temos 2 handlers: Home e Usuario
 	http.HandleFunc("/", Home(conect))
@@ -137,8 +137,9 @@ func Usuario(db *sql.DB) http.HandlerFunc {
 
 //conectarDB vai fazer a interface entre o servidor e banco de dados usando as informações de acesso armazenadas no .env
 func conectarDB() *sql.DB {
-	DBinfo := fmt.Sprintf("user=%s password=%s host=%s port=%v dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
-	db, err := sql.Open("pgx", DBinfo)
+	HDBInfo := fmt.Sprint("DATABASE_URL: postgres://nalhgjzhkoqwla:cf388a3bdad97cb7c38e947eb208035a00c99053a086dcf7c94d0d28e277ebe1@ec2-54-205-248-255.compute-1.amazonaws.com:5432/d4939969tmupml")
+	//DBinfo := fmt.Sprintf("user=%s password=%s host=%s port=%v dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	db, err := sql.Open("pgx", HDBInfo)
 	if err != nil {
 		panic(err)
 	}
